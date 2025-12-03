8ae81cba15fd6b16bad63c8da1cb762cb92710b9
Debbie Placio <debbieplacio@email.com>
2025-12-03 18:29:53 +0800

    add files to branch M1_ACTIVITY5

---
+++ src/bpi/java/module1/Activity5.java
@@ -0,0 +1,14 @@
+package bpi.java.module1;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for (int i = 1; i <= 50; i++) {
+			sum += i;
+		}
+		System.out.println("SUM = " + sum);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Module1Activity5 {
+}
