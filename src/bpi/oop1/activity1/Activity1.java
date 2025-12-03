8ff55db89540218d0408b70fe97624924b8ce1c7
Debbie Placio <debbieplacio@email.com>
2025-12-03 18:36:44 +0800

    add files to M2_ACTIVITY1

---
+++ src/bpi/oop1/activity1/Activity1.java
@@ -0,0 +1,13 @@
+package bpi.oop1.activity1;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book myBook = new Book();
+		myBook.title = "Harry Potter";
+		
+		myBook.checkout();
+	}
+
+}
---
+++ src/bpi/oop1/activity1/Book.java
@@ -0,0 +1,12 @@
+package bpi.oop1.activity1;
+
+public class Book {
+	String title = "Book Title";
+	
+	void checkout() {
+		System.out.print("Checked out book " + title );
+	}
+	
+
+
+}
