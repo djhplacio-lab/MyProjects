5745f6e6c4dbaf21e7b499e4ef8ab3014a18fd83
Debbie Placio <debbieplacio@email.com>
2025-12-04 16:31:53 +0800

    added files to branch M2_ACTIVITY2

---
+++ Seatwork2.java
@@ -0,0 +1,13 @@
+package bpi.oop1.activity2;
+
+public class Seatwork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Whiteboard wb = new Whiteboard();
+		int total = wb.addNumber(4, 4);
+		System.out.println("The total is: " + total);
+
+	}
+
+}
---
+++ Whiteboard.java
@@ -0,0 +1,9 @@
+package bpi.oop1.activity2;
+
+public class Whiteboard {
+	int addNumber(int addend1 , int addend2) {
+		int sum = addend1 + addend2;
+		return sum;
+	}
+
+}
